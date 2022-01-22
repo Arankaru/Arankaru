869d3a6984d76cf74c651edfc8d5a847f19cd4ba
Arankaru <58208105+Arankaru@users.noreply.github.com>
2022-01-22 19:33:14 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hello!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Add my discord lmaonade#3570
         </p>
       </main>
 
